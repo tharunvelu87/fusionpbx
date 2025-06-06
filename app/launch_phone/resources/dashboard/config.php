130afd90fa10ea1cc9957431180a45f7962d2f3d
Genius PBX <info@geniuspbx.com>
2025-06-06 10:57:49 +0530

    add launch_phone app and menu

---
+++ app/launch_phone/resources/dashboard/config.php
@@ -0,0 +1,46 @@
+<?php
+
+// Launch Phone Widget
+$array['dashboard'][$x]['dashboard_uuid'] = 'b79e2ef4-cc90-4890-83e7-eab88c5199d2'; // Use a unique UUID
+$array['dashboard'][$x]['dashboard_name'] = 'Launch Phone';
+$array['dashboard'][$x]['dashboard_path'] = 'launch_phone/launch_phone';
+$array['dashboard'][$x]['dashboard_icon'] = 'fa-phone-volume';
+$array['dashboard'][$x]['dashboard_icon_color'] = '#4caf50';
+$array['dashboard'][$x]['dashboard_url'] = '/app/launch_phone/resources/dashboard/launch_phone.php';
+$array['dashboard'][$x]['dashboard_target'] = 'self';
+$array['dashboard'][$x]['dashboard_width'] = '';
+$array['dashboard'][$x]['dashboard_height'] = '';
+$array['dashboard'][$x]['dashboard_content'] = '';
+$array['dashboard'][$x]['dashboard_content_text_align'] = 'center';
+$array['dashboard'][$x]['dashboard_content_details'] = '';
+$array['dashboard'][$x]['dashboard_chart_type'] = 'icon';
+$array['dashboard'][$x]['dashboard_label_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_label_text_color'] = '#444444';
+$array['dashboard'][$x]['dashboard_label_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_label_background_color'] = '';
+$array['dashboard'][$x]['dashboard_label_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_number_text_color'] = '#ffffff';
+$array['dashboard'][$x]['dashboard_number_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_number_background_color'] = '#4caf50';
+$array['dashboard'][$x]['dashboard_background_color'] = '#ffffff';
+$array['dashboard'][$x]['dashboard_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_detail_background_color'] = '';
+$array['dashboard'][$x]['dashboard_column_span'] = '1';
+$array['dashboard'][$x]['dashboard_row_span'] = '1';
+$array['dashboard'][$x]['dashboard_details_state'] = 'hidden';
+$array['dashboard'][$x]['dashboard_order'] = '80';
+$array['dashboard'][$x]['dashboard_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_description'] = 'Widget to launch WebRTC or SIP phone dialer.';
+$y = 0;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '85573555-7e18-4449-aa59-a264a7addda9'; // superadmin
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'b79e2ef4-cc90-4890-83e7-eab88c5199d2';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
+$y++;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'cde35063-58ca-4cdd-94fa-a1c4ee03e66c'; // admin
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'b79e2ef4-cc90-4890-83e7-eab88c5199d2';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'admin';
+$y++;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'b6e6c697-5351-484c-808d-884589c6d527'; // user
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'b79e2ef4-cc90-4890-83e7-eab88c5199d2';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'user';
+$x++;
